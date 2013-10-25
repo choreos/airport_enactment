d42020290a9257e93d85675b9a8c9a75d2a95ca9
leonardofl <leonardofl87@gmail.com>
2013-10-24 23:50:29 -0200

    editing json.node

--- chef-solo/solo.rb
+++ chef-solo/solo.rb
@@ -1,4 +1,4 @@
 file_cache_path "/tmp/chef-solo"
-cookbook_path "$HOME/chef-solo/cookbooks"
-json_attribs "$HOME/chef-solo/node.json"
+cookbook_path "$HOME/thales_enactment/chef-solo/cookbooks"
+json_attribs "$HOME/thales_enactment/chef-solo/node.json"
 
